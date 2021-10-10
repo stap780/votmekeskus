21183e0c8f2271789bf12f0dc48db07eea42172e
stap780 <panaet80@mail.ru>
2021-10-10 20:43:46 +0300

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :format, :pretty
 set :log_level, :info
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+after 'deploy:publishing', 'unicorn:restart'
 
 
 
