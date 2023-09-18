4faef6c0d78c47f87d02d9131be932835235db6b
stap780 <panaet80@mail.ru>
2023-09-18 19:37:48 +0300

    change worker process count

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,16 +3,16 @@ pid '/var/www/votmekeskus/current/tmp/pids/unicorn.pid'
 stderr_path '/var/www/votmekeskus/log/unicorn.log'
 stdout_path '/var/www/votmekeskus/log/unicorn.log'
 listen '/tmp/unicorn.votmekeskus.sock'
-worker_processes 1
+worker_processes 2
 timeout 30
 
-before_fork do |server, worker|
-  old_pid = "/var/www/votmekeskus/current/tmp/pids/unicorn.pid.oldbin"
-  if old_pid != server.pid
-    begin
-    sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-    Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
+# before_fork do |server, worker|
+#   old_pid = "/var/www/votmekeskus/current/tmp/pids/unicorn.pid.oldbin"
+#   if old_pid != server.pid
+#     begin
+#     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+#     Process.kill(sig, File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#     end
+#   end
+# end
