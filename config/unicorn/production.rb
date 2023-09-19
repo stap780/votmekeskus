49f57ed37a3c3533a8496455eb9d0fd3616c8fcf
stap780 <panaet80@mail.ru>
2023-09-19 08:58:02 +0300

    change unicorn workers

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,8 +3,8 @@ pid '/var/www/votmekeskus/current/tmp/pids/unicorn.pid'
 stderr_path '/var/www/votmekeskus/log/unicorn.log'
 stdout_path '/var/www/votmekeskus/log/unicorn.log'
 listen '/tmp/unicorn.votmekeskus.sock'
-worker_processes 2
-timeout 30
+worker_processes 4
+timeout 60
 
 before_fork do |server, worker|
   old_pid = "/var/www/votmekeskus/current/tmp/pids/unicorn.pid.oldbin"
