460637adae39749a017972c5ae29b6b5d2ae4478
stap780 <panaet80@mail.ru>
2023-12-14 12:56:57 +0300

    change server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -42,5 +42,5 @@
 
 # fetch(:default_env).merge!(rails_env: :production)
 
-server '138.197.65.61', user: 'deploy', roles: %w{app db web}
+server '138.197.6.185', user: 'deploy', roles: %w{app db web}
 set :unicorn_rack_env, -> { "production" }
