47ee84a996d9d4135e84a76c4e14074469e589dd
stap780 <panaet80@mail.ru>
2023-12-20 15:58:09 +0300

    change gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :branch, 'main'
 set :application, 'votmekeskus'
 set :repo_url, 'git@github.com:stap780/votmekeskus.git'
 set :deploy_to, '/var/www/votmekeskus'
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/storage.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :format, :pretty
 set :log_level, :info
